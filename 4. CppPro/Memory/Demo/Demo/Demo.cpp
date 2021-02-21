55cd4402a17ad0a659a5510eb450a6caac907d6a
zhaoguohan123 <zhao857133608@qq.com>
2021-02-21 21:06:33 +0800

    【updata】https://juejin.cn/post/6931701607226687501博客对于的案例

--- 4. CppPro/Memory/Demo/Demo/Demo.cpp
+++ 4. CppPro/Memory/Demo/Demo/Demo.cpp
@@ -28,30 +28,36 @@ void * PrintHello(void * threadarg)  // 线程函数用来输出被传入的参数
 void CallBackFun()               // 生成要传入线程的参数
 {
 	pthread_t threads[NUM_THREADS] = {0};
-	//struct thread_data td[NUM_THREADS] = {0};
-	struct thread_data *td[NUM_THREADS];                                   // 使用结构体指针数组，方便分配堆内存
+	struct thread_data td[NUM_THREADS] = {0};
+	//struct thread_data *td[NUM_THREADS];                                   // 使用结构体指针数组，方便分配堆内存
 	
 	int rc;
 	int i;
 
 	for( i=0; i < NUM_THREADS; i++ ){
-		td[i] = (struct thread_data *)malloc(sizeof(struct thread_data));   // 使用堆内存
+	//	td[i] = (struct thread_data *)malloc(sizeof(struct thread_data));   // 给结构体分配堆内存
 		cout <<"main() : creating thread, " << i << endl;
-		td[i]->thread_id = i;
+		td[i].thread_id = i;
+		td[i].message = (char * )"This is message";
+		rc = pthread_create(&threads[i], NULL,
+			PrintHello, (void *)&td[i]);
+	/*	td[i]->thread_id = i;
 		td[i]->message = (char * )"This is message";
 		rc = pthread_create(&threads[i], NULL,
-			PrintHello, (void *)td[i]);
+			PrintHello, (void *)td[i]);*/
 		if (rc){
 			cout << "Error:unable to create thread," << rc << endl;
 			exit(-1);
 		}
 	}
+	//Sleep(1000);
 }
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CallBackFun();
-	//Sleep(10000);
-	//pthread_exit(NULL);
+	
+	
 	getchar();
 	return 0;
 }
---
+++ 4. CppPro/Memory/README.md
@@ -0,0 +1,1 @@
+Demo对于的文章地址：https://juejin.cn/post/6931701607226687501
